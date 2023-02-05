6a9a8fdb2bd3dcae083763a3e4b3e9e649806899
Ei Cho <eicho@unicomi.com>
2023-02-05 20:46:21 +0630

    update contents

--- src/App.js
+++ src/App.js
@@ -4,30 +4,38 @@ const App = () => {
     {
       id: 1,
       title: "Men Clothes",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
     {
       id: 2,
       title: "Women Clothes",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
       id: 3,
-      title: "Fashion Accessories",
+      title: "Shoes",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     },
     {
       id: 4,
-      title: "Computers & Laptops",
+      title: "Women Fashion",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
       id: 5,
-      title: "Mobile & Gadgets",
+      title: "Men Fashion",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
   ];
   return (
     <div className="categories-wrapper">
-      {categories.map(({ title, id }) => (
+      {categories.map(({ title, id, imageUrl }) => (
         //{ title } =destructuring instead of 'category'
         <div key={id} className="category-wrapper">
-          <div className="background-image" />
+          <div
+            className="background-image"
+            style={{ backgroundImage: `url(${imageUrl})` }}
+          />
           <div className="category-body-wrapper">
             <h2>{title}</h2>
             <p>Shop Now</p>
