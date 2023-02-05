3a356a8c6f0b51b1adca7a9b7540b434fcf2202c
Ei Cho <eicho@unicomi.com>
2023-02-05 20:21:21 +0630

    setting up categories

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./categories.styles.scss";
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Men Clothes",
+    },
+    {
+      id: 2,
+      title: "Women Clothes",
+    },
+    {
+      id: 3,
+      title: "Fashion Accessories",
+    },
+    {
+      id: 4,
+      title: "Computers & Laptops",
+    },
+    {
+      id: 5,
+      title: "Mobile & Gadgets",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-wrapper">
+      {categories.map(({ title, id }) => (
+        //{ title } =destructuring instead of 'category'
+        <div key={id} className="category-wrapper">
+          <div className="background-image" />
+          <div className="category-body-wrapper">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
