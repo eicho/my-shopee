ce09fff7739b4b0414d5ad70e035fde8e5072f37
Ei Cho <eicho@unicomi.com>
2023-02-06 13:42:26 +0630

    setting up homepage

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
-import Directory from "./components/directory/directory.component";
+import { Routes, Route } from "react-router";
+import Home from "./routes/home/home.component";
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Men Clothes",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-    {
-      id: 2,
-      title: "Women Clothes",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "Shoes",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "Women Fashion",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "Men Fashion",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-  ];
-  return <Directory categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,33 @@
+import Directory from "../../components/directory/directory.component";
+const Home = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Men Clothes",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    },
+    {
+      id: 2,
+      title: "Women Clothes",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: 3,
+      title: "Shoes",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: 4,
+      title: "Women Fashion",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      id: 5,
+      title: "Men Fashion",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    },
+  ];
+  return <Directory categories={categories} />;
+};
+
+export default Home;
