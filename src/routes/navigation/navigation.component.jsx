985bb26b1f52763ae2cdd5b41d5b13f656aed614
Ei Cho <32412506+eicho@users.noreply.github.com>
2023-02-09 11:14:54 +0630

    create Cart dropdown component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,6 +2,7 @@ import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 
 import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { ReactComponent as ShopLogo } from "../../assets/shopee.svg";
 
 import { UserContext } from "../../contexts/user.context";
@@ -42,6 +43,7 @@ const Navigation = () => {
           )}
           <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </Fragment>
