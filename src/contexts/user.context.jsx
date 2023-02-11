623c6cf4b9d7d7373d1abc0ee52df6d5d244fd5c
Ei Cho <32412506+eicho@users.noreply.github.com>
2023-02-11 20:33:23 +0630

    updating user Context to useReducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,10 +1,36 @@
 //export both the 'context' and the 'provider'
 //'provider' is the actual component
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
 } from "../utils/firebase/firebase.utils";
+
+// an object which has these key value pairs where it's the actual Value.
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+//'INITIAL_STATE' is an object and current user is equal to null because when initialize the application, have no current user.
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+const userReducer = (state, action) => {
+  // console.log("dispatched"); //call 'dispatch' whenever 'userReducer' run
+  // console.log(action);
+
+  const { type, payload } = action;
+
+  // switch statement is a special block of code where pass the switch, some value or passing type.
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      //give me the same values on the previous state object that you had, but anything afterwards going to overwrite
+      return { ...state, currentUser: payload };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
 
 //as the actual value u wanna access
 //This user context object is going to give us back whatever value is passed in for the value.
@@ -16,8 +42,14 @@ export const UserContext = createContext({
 //literal functional component
 //.provider will wrap around any other components that need access to the values inside
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
-  const value = { currentUser, setCurrentUser }; //set of function,actual value
+  //Whenever 'dispatch' gets called and a new 'state' object is returned, then will rerun this functional component.
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE); //'useReducer' as a hook.'useReducer' has 2 arguments.(userreducer or some reducer,initial value of the state).'dispatch' will take 'action' object and then pass it in
+  // console.log(currentUser);
+
+  const setCurrentUser = (user) =>
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+
+  const value = { currentUser, setCurrentUser };
 
   //with use effect, with this callback, run whatever return from this callback when it mounts.
   useEffect(() => {
